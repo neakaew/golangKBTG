31fc584b31c2dca03cd499f0b4da5f73b5a738c6
Udom039 <pokemoner799@gmail.com>
2019-06-22 16:05:04 +0700

    Add Project traning golangKBTG

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+type Todo struct {
+	ID     int
+	Title  string
+	Status string
+}
+
+func getTodos(c *gin.Context) {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("SELECT id, title, status FROM todos")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	todos := []Todo{}
+
+	for rows.Next() {
+		t := Todo{}
+
+		err := rows.Scan(&t.ID, &t.Title, &t.Status)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		todos = append(todos, t)
+	}
+	c.JSON(http.StatusOK, todos)
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/api/todos", getTodos)
+	r.Run(":2134")
+	fmt.Print("start server .........")
+}
