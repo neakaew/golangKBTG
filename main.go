613e55b7be3010d80f4e3bd2527867bebf880a40
Udom039 <pokemoner799@gmail.com>
2019-06-22 20:55:07 +0700

    change post

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
 type Todo struct {
-	ID     int
-	Title  string
-	Status string
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
 }
 
 func getTodos(c *gin.Context) {
@@ -22,9 +23,8 @@ func getTodos(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-	defer db.Close()
 
-	stmt, err := db.Prepare("SELECT id, title, status FROM todos")
+	stmt, err := db.Prepare("SELECT id, title, status FROM todos ORDER BY id ASC")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -49,12 +49,86 @@ func getTodos(c *gin.Context) {
 		todos = append(todos, t)
 	}
 	c.JSON(http.StatusOK, todos)
+	defer db.Close()
+}
+
+func getTodosByIdHandler(c *gin.Context) {
+	idParam := c.Param("id") // รับ paramiter id
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	db, _ := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	stmt, _ := db.Prepare("SELECT id, title, status FROM todos WHERE id=$1")
+
+	row := stmt.QueryRow(id)
+	t := Todo{}
+
+	err2 := row.Scan(&t.ID, &t.Title, &t.Status)
+	if err2 != nil {
+		log.Fatal("error..", err2.Error())
+	}
+
+	c.JSON(http.StatusOK, t)
+	defer db.Close()
+}
+
+func postTodos(c *gin.Context) {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	t := &Todo{}
+	if err := c.BindJSON(t); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var id int
+	query := "INSERT INTO todos (title, status) VALUES ($1, $2) RETURNING id"
+	row := db.QueryRow(query, &t.Title, &t.Status)
+	err = row.Scan(&id)
+	if err != nil {
+		log.Fatal(err.Error(), id)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title":  t.Title,
+		"status": t.Status,
+	})
+	defer db.Close()
+}
+
+func deleteTodosByIdHandler(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	stmt, err := db.Prepare("DELETE FROM todos WHERE id=$1")
+	rs, err := stmt.Exec(id)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	c.JSON(http.StatusOK, rs)
+	defer db.Close()
 }
 
 func main() {
 	r := gin.Default()
 
 	r.GET("/api/todos", getTodos)
-	r.Run(":2134")
-	fmt.Print("start server .........")
+	r.GET("/api/todos/:id", getTodosByIdHandler)
+	r.POST("/api/todos", postTodos)
+	r.DELETE("/api/seleceTodos/:id", deleteTodosByIdHandler)
+	r.Run(":3434")
 }
